d10750a3e71a4679f9b0902c5ca7f9fd8b4c98be
Felicia-Fu <FFC0340@hotmail.com>
2026-01-07 11:44:41 -0500

    Created the project

---
+++ src/threekingdomslegacy/Game.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package threekingdomslegacy;
+import processing.core.PApplet;
+/**
+ *
+ * @author FFC03
+ */
+public class Game extends PApplet{
+    public void settings(){
+        size(400, 300);
+    }
+    public void setup(){
+        
+    }
+    public void draw(){
+        
+    }
+    
+}
