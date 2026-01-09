2c5c92b39dbc06388a6369863bdacaf94f76f949
Felicia-Fu <FFC0340@hotmail.com>
2026-01-09 10:07:58 -0500

    Added user login page (partially done)

--- src/threekingdomslegacy/Game.java
+++ src/threekingdomslegacy/Game.java
@@ -4,6 +4,7 @@
  */
 package threekingdomslegacy;
 import processing.core.*;
+import java.io.*;
 /**
  *
  * @author FFC03
@@ -11,54 +12,112 @@ import processing.core.*;
 public class Game extends PApplet{
     private int stage = -1;
     private int index = 1;
+    private final int screenWidth = 700, screenHeight = 500;
+    private PFont titleFont, subtitleFont, contentFont;
+    private String username = "";
+    private String password = "";
+    private String placeholder = "";
+    private boolean finishUsername = false;
+    private int count = 0;
     public void settings(){
-        size(500, 400);
+        size(screenWidth, screenHeight);
     }
     public void setup(){
         background(255);
+        titleFont = createFont("fonts/rogenz.otf", 120);
+        subtitleFont = createFont("fonts/rogenz.otf", 50);
+        contentFont = createFont("fonts/rogenz.otf", 25);
+        
     }
     public void draw(){
         switch (stage){
             case -1:
                 fill(255, 0, 0);
-                textSize(40);
-                PFont font = createFont("fonts/rogenz.otf", 50);
-                textFont(font);
+                textFont(subtitleFont);
                 textAlign(CENTER);
-                text("Three Kingdoms", 250, 150);
+                text("Three Kingdoms", screenWidth/2, 150);
                 fill(0);
-                text("Legacy", 250, 200);
+                textFont(titleFont);
+                text("Legacy", screenWidth/2, 250);
                 fill(23, 100, 100);
-                rect(250 - 100, 300 - 74/2, 200, 74);
+                rect(screenWidth/2 - 100, screenHeight/2 + 74/2, 200, 74);
                 fill(0);
-                text("Start", 250, 320); 
+                textFont(subtitleFont);
+                text("Start", screenWidth/2, screenHeight/2 + 93); 
                 break;
             case 0:
+                background(255);
+                textFont(contentFont);
+                textAlign(LEFT);
+                text("Enter username: ", 150, 100);
+                text(username, 400, 100);
+                text("Enter password: ", 150, 150);
+                text(placeholder, 400, 150);
+                break;
+            case 1:
                 background(255);
                 fill(0);
                 textAlign(CENTER);
                 String texts = index == 1 ? "Shu" : index == 2 ? "Wei" : "Eastern Wu";
-                text(texts, 250, 200);
+                text(texts, screenWidth/2, screenHeight/2);
                 break;
         }
-        System.out.println(stage);
         
     }
     public void mousePressed(){
-        if (stage == -1 && mouseX < 350 && mouseX > 150 && mouseY < 337 && mouseY > 263){
+        if (stage == -1 && mouseX < screenWidth/2 + 100 && mouseX > screenWidth/2 - 100 && mouseY < screenHeight/2 + 74 * 3 / 2 && mouseY > screenHeight/2 + 74/2){
             stage = 0;
         }
     }
     public void keyPressed(){
-        if (stage == 0){
-            if (keyCode == LEFT){
-                index = (index - 1) % 3;
-                if (index < 0){
-                    index += 3;
+        switch(stage){
+            case 0:
+                if (keyCode != ENTER){
+                    if (keyCode != CODED){
+                        if (count == 0){
+                            username += key;
+                        } else{
+                            password += key;
+                            placeholder += "0";
+                        }
+                    }
+                    if (keyCode == BACKSPACE){
+                        if (count == 0){
+                            username = username.length() <= 1 ? "" : username.substring(0, username.length() - 2);
+                        } else{
+                            password = password.length() <= 1 ? "" : password.substring(0, password.length() - 2);
+                            placeholder = placeholder.length() <= 1 ? "" : placeholder.substring(0, placeholder.length() - 2);
+                        }
+                    }
+                } else{
+                    if ((count == 0 && !username.isEmpty()) || (count == 1 && !password.isEmpty())){
+                        count ++;
+                    }
+                    if (count == 2){
+                        count = 0;
+                        stage = 1;
+                        try{
+                            FileWriter fw = new FileWriter("users.csv", true);
+                            PrintWriter pw = new PrintWriter(fw);
+                            pw.write(username + "," + password);
+                            pw.close();
+                        } catch (IOException e){
+                            text("Error occured while processing", screenWidth/2, screenHeight/2);
+                        }
+                    }
                 }
-            } else if (keyCode == RIGHT){
-                index = index % 3 + 1;
-            }
+                
+                break;
+            case 1:
+                if (keyCode == LEFT){
+                    index = (index - 1) % 3;
+                    if (index < 0){
+                        index += 3;
+                    }
+                } else if (keyCode == RIGHT){
+                    index = index % 3 + 1;
+                }
+                break;
         }
     }
     
